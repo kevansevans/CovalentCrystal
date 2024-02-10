8d7eff17f2d23aa4d812801f0de8355ed2ae6433
Kaelan <kbeevans@gmail.com>
2024-02-09 20:45:32 -0700

    Export NPC ZScript ednums

--- src/doom/EdNumBuilder.hx
+++ src/doom/EdNumBuilder.hx
@@ -3,6 +3,7 @@ package doom;
 import sys.io.File;
 import haxe.io.Bytes;
 import crystal.BasePokemonData;
+import crystal.BaseOverworldData;
 
 /**
  * ...
@@ -14,21 +15,29 @@ class EdNumBuilder
 	public function new() 
 	{
 		var output = "DoomEdNums\n{\n";
-		var index = 10000;
+		
+		var pkindex = 10000;
+		var ovindex = 20000;
 		
 		for (pk in BasePokemonData.info)
 		{
 			if (pk.name == "AASHITTY") continue;
 			if (pk.name != "Unown")
 			{
-				output += '\t${index} = PK_${pk.name}\n';
+				output += '\t${pkindex} = PK_${pk.name}\n';
 			}
-			++index;
+			++pkindex;
+		}
+		
+		for (ov in BaseOverworldData.info)
+		{
+			output += '\t${ovindex} = PK_${ov.name}NPC\n';
+			++ovindex;
 		}
 		
 		output += '}';
 		
-		var out = File.write('./wad/MAPINFO.PokeEdNums');
+		var out = File.write('./wad/MAPINFO.EdNums');
 		out.write(Bytes.ofString(output));
 		out.close();
 	}
